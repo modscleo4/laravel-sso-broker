f711679d8114761c6aee9bf2e534eae0205d3dd4
Dhiego Cassiano Fogaça Barbosa <modscleo4@outlook.com>
2019-09-24 12:10:27 -0300

    Add Files

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Broker;
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+    protected function attemptLogin(Request $request)
+    {
+        $credentials = $this->credentials($request);
+        if (config('broker.useSSO')) {
+            $broker = new Broker;
+
+            return $broker->login(
+                $credentials[$this->username()], $credentials['password'], $request->filled('remember')
+            );
+        } else {
+            return $this->guard()->attempt(
+                $this->credentials($request), $request->filled('remember')
+            );
+        }
+    }
+
+    protected function logout(Request $request)
+    {
+        if (config('broker.useSSO')) {
+            $broker = new Broker;
+
+            $broker->logout();
+        }
+
+        $this->guard()->logout();
+
+        $request->session()->invalidate();
+
+        return $this->loggedOut($request) ?: redirect('/');
+    }
+}
